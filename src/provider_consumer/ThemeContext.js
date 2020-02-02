db757f49cd4498413e7ed0b9cfa031c20f59e5d4
zengchao <cengchao@hzxt.weibangong.com>
2020-02-02 22:25:41 +0800

    initial

--- src/provider_consumer/ThemeContext.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const ThemeContext = React.createContext({textColor: 'green'});
-const {Provider: ThemProvider, Consumer: ThemeConsumer} = ThemeContext;
-export {
-    ThemeConsumer,
-    ThemProvider
-};
